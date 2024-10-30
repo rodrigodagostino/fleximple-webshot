9db5ee0084f0bd35232550a3d1c2c4a7911f0e3f
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2024-10-30 11:04:33 -0300

    refactor: improve Puppeteer performance

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -73,7 +73,33 @@ export const actions: Actions = {
 		const browser = await puppeteer.launch({
 			headless: true,
 			ignoreDefaultArgs: ['--disable--extensions'],
-			args: ['--no-sandbox', '--disable-setuid-sandbox'],
+			args: [
+				'--no-sandbox',
+				'--disable-setuid-sandbox',
+				// Disable cache.
+				'--aggressive-cache-discard',
+				'--disable-cache',
+				'--disable-application-cache',
+				'--disable-offline-load-stale-cache',
+				'--disable-gpu-shader-disk-cache',
+				'--media-cache-size=0',
+				'--disk-cache-size=0',
+				'--disable-extensions',
+				// Speed up.
+				'--disable-component-extensions-with-background-pages',
+				'--disable-default-apps',
+				'--mute-audio',
+				'--no-default-browser-check',
+				'--autoplay-policy=user-gesture-required',
+				'--disable-background-timer-throttling',
+				'--disable-backgrounding-occluded-windows',
+				'--disable-notifications',
+				'--disable-background-networking',
+				'--disable-breakpad',
+				'--disable-component-update',
+				'--disable-domain-reliability',
+				'--disable-sync',
+			],
 		});
 
 		// Open page.
